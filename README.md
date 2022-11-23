1996c1e40a8931ff1432bd9466b0da0b4e99f404
SahanaHV <sahanasonu339@gmail.com>
2022-11-23 13:17:10 +0530

    Created Layout Component

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 2. Install required Development Tools
 3. Create Next App
 4. Publish code to Github
+
+5. Create website Layout
+   1. create layout component
+   2. add header
+   3. add main section
+   4. add footer
+   5. add tailwind classes
---
+++ components/Layout.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ title, Children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Sara' : 'Sara'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex justify-between h-12 shadow-md items-center px-4 ">
+            <Link href="/" className="text-lg font-bold">
+              Sara
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{Children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright @ 2022 Sara</p>
+        </footer>
+      </div>
+    </>
+  );
+}
